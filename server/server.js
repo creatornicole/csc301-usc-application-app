21d173df0181066fc1d5f2585e82f5f654b5e467
creatornicole <ngottschall@web.de>
2024-05-29 09:45:40 +1000

    adds backend validation and prevents sql injection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 // dockerized in own container
 const express = require('express');
 const cors = require('cors');
+// check is the primary function used to validate and sanitize input
+// validationResult is a function of the req and is used to return an obj containing all validation errors
+const { check, validationResult } = require('express-validator'); // to validate and sanitize data
 const client = require('./db.js'); // database file
 const port = process.env.PORT; // define/find PORT inside .env
 
@@ -20,11 +23,10 @@ const createTable = async() => {
                 birthdate DATE,
                 phonenumber VARCHAR(50),
                 address VARCHAR(100),
-                abbr VARCHAR(25),
+                abbr VARCHAR(50),
                 course VARCHAR(100),
                 seminargroup VARCHAR(25),
-                
-                const VARCHAR(50)
+                position VARCHAR(100)
             )
         `);
         console.log('Table "applications" up and running!');
@@ -48,19 +50,64 @@ app.get('/applications', async (req, res) => {
     }
 })
 
+// validates data to correspond to database definition
+const applicationValidation = [
+    check('name')
+        .trim()
+        .isLength({ max: 100 }).withMessage('Input name must be at most 100 characters long')
+        .escape(),
+    check('birthdate')
+        .trim()
+        .isISO8601().withMessage('Invalid date format, should be YYYY-MM-DD')
+        .escape(),
+    check('phonenumber')
+        .trim()
+        .isLength({ max: 50 }).withMessage('Input phonenumber must be at most 50 characters long')
+        .escape(),
+    check('address')
+        .trim()
+        .isLength({ max: 100 }).withMessage('Input address must be at most 100 characters long')
+        .escape(),
+    check('abbr') // is university mail address
+        .trim()
+        .isEmail().withMessage('Invalid email address')
+        .isLength({ max: 50 }).withMessage('Input abbr must be at most 50 characters long')
+        .normalizeEmail(),
+    check('course')
+        .trim()
+        .isLength({ max: 100 }).withMessage('Input course must be at most 100 characters long')
+        .escape(),
+    check('seminargroup')
+        .trim()
+        .isLength({ max: 25 }).withMessage('Input seminargroup must be at most 25 characters long')
+        .escape(),
+    check('position')
+        .trim()
+        .isLength({ max: 100 }).withMessage('Input position must be at most 100 characters long')
+        .escape(),
+];
+
 // test route to store new application
-app.post('/apply', async (req, res) => {
-    // TODO: add data validation to prevent SQL injection attacks
-    // validate and sanitize user input before using
-    // use paramterized queries or an ORM (Object-Relational Mapping)
-    // -> see course material
+// TODO: PREVENT SQL INJECTION, make sure that characters are not more than defined in the db definition
+app.post('/apply', applicationValidation, async (req, res) => {
+    // TODO: abbr -> get first part
+
+    // gets errors according to validation
+    const errors = validationResult(req);
+
+    // respond with error code in case of invalid data
+    if (!errors.isEmpty()) { // NOT WORKING PROPERLY??
+        return res.status(422).json({ errors: errors.array() });
+    }
+
     const { name, birthdate, phonenumber, address, abbr, course, seminargroup, position } = req.body
-    console.log(req.body);
+    
     try {
+        // parameterized queries to prevent sql injection
         await client.query('INSERT INTO applications (name, birthdate, phonenumber, address, abbr, course, seminargroup, position)'
             + ' VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
             [name, birthdate, phonenumber, address, abbr, course, seminargroup, position]);
-        res.status(200).send('Successfully added child!');
+        res.status(200).send('Successfully added application!');
     } catch (err) {
         console.log(err)
         res.status(500).send('Internal Server Error');
@@ -82,3 +129,26 @@ app.delete('/delete', async (req, res) => {
 app.listen(port, () => console.log(
     `Server has started on port: ${port}`
 ))
+
+// Validation Array
+var loginValidate = [
+  // Check Username
+  check('username', 'Username Must Be an Email Address').isEmail()
+  .trim().escape().normalizeEmail(),
+  // Check Password
+  check('password').isLength({ min: 8 }).withMessage('Password Must Be at Least 8 Characters').matches('[0-9]').withMessage('Password Must Contain a Number').matches('[A-Z]').withMessage('Password Must Contain an Uppercase Letter').trim().escape()];
+
+
+
+app.post('/login', loginValidate, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+  	return res.status(422).json({ errors: errors.array() });
+  }
+  else {
+. // Insert Login Code Here
+. let username = req.body.username;
+. let password = req.body.password;
+. res.send(`Username: ${username} Password: ${password}`);
+  }
+});
